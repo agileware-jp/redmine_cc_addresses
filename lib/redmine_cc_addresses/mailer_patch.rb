a3087eb740664d1284d9a19409bbb5f51a3140af
Alex Klimenkov <alex.klimenkov89@gmail.com>
2014-05-22 14:27:53 +0300

    fix to 2.5 redmine

--- lib/redmine_cc_addresses/mailer_patch.rb
+++ lib/redmine_cc_addresses/mailer_patch.rb
@@ -13,16 +13,16 @@ module RedmineCcAddresses
   end
 
   module MailerInstanceMethods
-    def issue_edit_with_cc_addresses(journal)
+    def issue_edit_with_cc_addresses(journal, recipients, cc)
       issue = journal.journalized.reload
-      issue_edit_without_cc_addresses(journal)
+      issue_edit_without_cc_addresses(journal, recipients, cc)
       unless journal.do_not_send_cc?
         cc_addresses = issue.cc_addresses.collect {|m| m.mail}
         headers[:cc] << cc_addresses
       end
     end
-    def issue_add_with_cc_addresses(issue)
-      issue_add_without_cc_addresses(issue)
+    def issue_add_with_cc_addresses(issue, recipients, cc)
+      issue_add_without_cc_addresses(issue, recipients, cc)
       cc_addresses = issue.cc_addresses.collect {|m| m.mail}
       headers[:cc] << cc_addresses
     end
